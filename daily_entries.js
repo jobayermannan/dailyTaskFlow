7749a9614c4f0e8185e9a6660ae61448cd76e6e4
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:23:56 +0600

    Daily update 72 - 2024-12-14T07:23:56.025Z

--- daily_entries.js
+++ daily_entries.js
@@ -861,3 +861,14 @@ function isPrime(num) {
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
 }
+
+// Entry added at 2024-12-14T07:23:56.024Z
+// Topic: Prime Numbers
+
+// Function to check if a number is prime
+function isPrime(num) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) return false;
+    }
+    return num > 1;
+}
