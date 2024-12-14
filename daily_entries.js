bf11ebdb20cd631a028f1b61d32afbec33bdc209
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:22:59 +0600

    Daily update 60 - 2024-12-14T07:22:59.653Z

--- daily_entries.js
+++ daily_entries.js
@@ -734,3 +734,16 @@ class Block {
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
 }
+
+// Entry added at 2024-12-14T07:22:59.652Z
+// Topic: Blockchain
+
+// Simple blockchain block structure
+class Block {
+    constructor(timestamp, data, previousHash = '') {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+}
