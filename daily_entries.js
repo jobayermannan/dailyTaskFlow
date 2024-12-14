271df5a827e024f59f2cb0a313a52dbff49c6959
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:17:48 +0600

    Daily update 2 - 2024-12-14T07:17:47.852Z

--- daily_entries.js
+++ daily_entries.js
@@ -129,3 +129,14 @@ class Block {
         this.hash = this.calculateHash();
     }
 }
+
+// Entry added at 2024-12-14T07:17:47.852Z
+// Topic: Prime Numbers
+
+// Function to check if a number is prime
+function isPrime(num) {
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) return false;
+    }
+    return num > 1;
+}
