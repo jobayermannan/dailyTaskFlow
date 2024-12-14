6c1d14a44fe4e62b299f2738f43e94ba27d8dea5
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:21:16 +0600

    Daily update 41 - 2024-12-14T07:21:16.854Z

--- daily_entries.js
+++ daily_entries.js
@@ -539,3 +539,11 @@ class Block {
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
 }
+
+// Entry added at 2024-12-14T07:21:16.853Z
+// Topic: AI
+
+// Simple neural network activation function
+function sigmoid(x) {
+    return 1 / (1 + Math.exp(-x));
+}
