2470f2fe587d085a1734c7e64354862f84b97f8a
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:17:59 +0600

    Daily update 4 - 2024-12-14T07:17:59.433Z

--- daily_entries.js
+++ daily_entries.js
@@ -151,3 +151,11 @@ function isPrime(num) {
     }
     return num > 1;
 }
+
+// Entry added at 2024-12-14T07:17:59.433Z
+// Topic: AI
+
+// Simple neural network activation function
+function sigmoid(x) {
+    return 1 / (1 + Math.exp(-x));
+}
