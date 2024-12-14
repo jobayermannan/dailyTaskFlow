ef86251ab317256a6478c69f43c8d20426745ca8
jobayermannan <jobayermannan777@gmail.com>
2024-12-14 13:26:13 +0600

    Daily update 97 - 2024-12-14T07:26:13.094Z

--- daily_entries.js
+++ daily_entries.js
@@ -1145,3 +1145,16 @@ class Block {
         this.hash = this.calculateHash();
     }
 }
+
+// Entry added at 2024-12-14T07:26:13.093Z
+// Topic: Blockchain
+
+// Simple blockchain block structure
+class Block {
+    constructor(timestamp, data, previousHash = '') {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+    }
+}
